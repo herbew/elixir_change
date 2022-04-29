0f3859c42a5de12860f3805a2c8a86c06fac6cd9
Herbew <herbew@gmail.com>
2022-04-29 23:02:01 +0700

    test

--- lib/changes.ex
+++ lib/changes.ex
@@ -1,7 +1,7 @@
 defmodule Change do
 	@doc """
 	Determine the least number of coins to be given to the user such
-	that the sum of the coins' value would equal the correct amount of change.
+	that the sum of the coins' value would equal the correct amount of map_change.
 	It returns :error if it is not possible to compute the right amount of coins.
 	Otherwise returns the tuple {:ok, map_of_coins}
 	
@@ -16,9 +16,49 @@ defmodule Change do
 	"""
 	
 	@spec generate(integer, list) :: {:ok, map} | :error
-	def generate(n, list) do
+	def generate(amount, values) do
 		cond do
-		  n == nil or  list == [] -> :error
+		  amount == nil or amount <= 0 or values == [] -> :error
+		  true -> 
+			  change(
+				   amount,
+				   
+				   # sort(enumerable, fun)
+				   # Sorts the enumerable by the given function.
+				   # &(&2 < &1) is anonymous function if params 2 < params 1
+				   Enum.sort(values, &(&2 < &1)),
+				   
+				   # into(enumerable, collectable, transform)
+				   # Inserts the given enumerable into a collectable according to the transformation function.
+				   Enum.into(values, %{ }, &{&1, 0})
+			 )
+		end
+		 
+	end
+	
+	# Private function 3 params (
+	# amount is 0, 
+	# list<Used to ignore an argument or part of a pattern match expression>, 
+	# map)
+	defp change(0, _values, map_change), do: {:ok, map_change}
+	
+	# Private function 3 params (amount, list, map)
+	defp change(amount, values, map_change) do
+		# find(enumerable, default \\ nil, fun)
+		# Returns the first element for which fun returns a truthy value. 
+		# If no such element is found, returns default.
+		case Enum.find(values, fn value -> value <= amount end) do
+			nil ->
+			  :error
+			coin ->
+			  change(
+			    amount - coin,
+			    values,
+			    # update!(map, key, fun)
+			    # &(&1 + 1) is anonymous function fn(x) -> x + 1 end
+			    Map.update!(map_change, coin, &(&1 + 1))
+			  )
 		end
 	end
+  
 end
--- test/changes_test.exs
+++ test/changes_test.exs
@@ -1,10 +1,34 @@
 defmodule ChangeTest do
     use ExUnit.Case
     
-	test "1. No cent or and no coins" do
-	  assert Change.generate(nil, []) == :error
-	  assert Change.generate(1, []) == :error
-	  assert Change.generate([nil [1]) == :error
-	end #test "1. No cent or and no coins" do
+	test "1. No amount or and no coins" do
+		assert Change.generate(nil, []) == :error
+		assert Change.generate(1, []) == :error
+		assert Change.generate(nil, [1]) == :error
+	end #test "1. No amount or and no coins" do
+	
+	@tag :pending
+	test "2. The flexible coins with 1, [1,3,10]" do
+		coins = [1,3,10]
+		assert Change.generate(1, coins) == {:ok, %{1 => 1, 3 => 0, 10 => 0}}
+		assert Change.generate(8, coins) == {:ok, %{1 => 2, 3 => 2, 10 => 0}}
+		assert Change.generate(14, coins) == {:ok, %{1 => 1, 3 => 1, 10 => 1}}
+	end #test "2. The flexible coins with 1, [1,3,10]" do
+	
+	@tag :pending
+	test "3. The coins with error, [2,3,10]" do
+		coins = [2,3,10]
+		assert Change.generate(1, coins) == :error
+		assert Change.generate(7, coins) == :error
+		assert Change.generate(14, coins) == :error
+	end #test "3. The coins with error, [2,3,10]" do
+	
+	@tag :pending
+	test "4. The coins with mix, [2,-3,10]" do
+		coins = [2,-3,10]
+		assert Change.generate(7, coins) == {:ok, %{-3 => 1, 2 => 5, 10 => 0}}
+		assert Change.generate(11, coins) == {:ok, %{-3 => 1, 2 => 2, 10 => 1}}
+		assert Change.generate(57, coins) == {:ok, %{-3 => 1, 2 => 5, 10 => 5}}
+	end #test "4. The coins with mix, [2,-3,10]" do
 	
 end
